ff315aa2696203de8b3af40534478c60b71ba8ee
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-01-27 15:37:17 +0530

    latest

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
-
+const port = 8000;      
 app.get('/ping', (req, res) => {
     res.send('Pong');
     }
 );
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is running on ${port}`);
     }
 );
